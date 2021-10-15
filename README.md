a5db6acce7ce2aba5dae0d2d48b9ae16b5d4af07
orkestral <joe.dutra.vm@gmail.com>
2021-10-15 13:16:19 -0300

    new feature

---
+++ README.md
@@ -0,0 +1,1 @@
+# Decrypt Remote
---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import { decryptMedia } from './decrypt';
+const mime = require('mime-types');
+const fs = require('fs');
+
+
+const processMessage = async message => {
+    if (message.mimetype) {
+      const filename = `${message.t}.${mime.extension(message.mimetype)}`;
+      const mediaData = await decryptMedia(message);
+      const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString(
+        'base64'
+      )}`;
+      fs.writeFile(filename, mediaData, function(err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log('The file was saved!');
+      });
+    }
+  }
+
+
+export function decrypt(file){
+
+    let typee = file.mimetype.split('/')[0]
+
+    let type = 'DOCUMENT'
+
+    switch (typee[0]) {
+        case 'audio':
+            type = 'AUDIO'
+            break;
+        case 'image':
+            type = 'IMAGE'
+            break;
+        case 'video':
+            type = 'VIDEO'
+            break;
+   
+    }
+
+    let message = {
+    mediaKey: file.mediaKey.toString('base64'),
+    filehash: file.fileSha256,
+    mimetype: file.mimetype,
+    type: type,
+    size: file.fileLength.low
+    }
+
+    return processMessage(message)
+}
